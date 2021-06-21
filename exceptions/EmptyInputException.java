177250ec3ec8c464d048b1512f7aca66d42471ee
KiranmaiParachuri <86102607+KiranmaiParachuri@users.noreply.github.com>
2021-06-21 16:54:10 +0530

    Add files via upload

---
+++ exceptions/EmptyInputException.java
@@ -0,0 +1,7 @@
+package com.example.springdatajpa.learnspringdatajpa.exceptions;
+
+public class EmptyInputException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ exceptions/GenresNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.springdatajpa.learnspringdatajpa.exceptions;
+
+public class GenresNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+}
